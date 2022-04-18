36109bde9e595fcb62daaf27df75f3a796977c7d
roman7373 <rra83ul@mail.ru>
2022-04-18 12:47:01 +0400

    deploy31

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -91,9 +91,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-
+STATIC_URL = "/static1/"
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
-STATIC_ROOT = os.path.join(BASE_DIR, './static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static1/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
